c177431a4087c8f5953b58c01beebe8bca12c85e
BrenoCSenna <100354416+BrenoCSenna@users.noreply.github.com>
2023-08-20 11:22:10 -0300

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Fast_Foodly
+namespace Categoria
 {
     /// <summary>
     /// Interação lógica para MainWindow.xam
@@ -24,9 +23,11 @@ namespace Fast_Foodly
         public MainWindow()
         {
             InitializeComponent();
-            string sqlConnectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
-            var database = new FastFoodly.DatabaseService(sqlConnectionString);
-            var cardapio = database.ConsultaCardapio();
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -34,12 +35,17 @@ namespace Fast_Foodly
 
         }
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
         {
 
         }
 
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_3(object sender, TextChangedEventArgs e)
         {
 
         }
