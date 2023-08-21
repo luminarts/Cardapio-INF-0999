02721d1ce2b0542cef67134d3b408b6ddae98ab9
Gustavo Borba <gustavo.borba2001@gmail.com>
2023-08-21 12:23:28 -0300

    fix: add-on rectangles

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Cardapio_INF_0999
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
