cc6dd691dae9a7bb443fb54da72a99c2c97334ec
sathyagimenes <sathya.gimenes@gmail.com>
2023-08-22 20:34:59 -0300

    WIP: button biding addCartItem

--- ViewModel/AddProductViewModel.cs
+++ ViewModel/AddProductViewModel.cs
@@ -4,5 +4,30 @@ namespace FastFoodly.ViewModel;
 
 public class AddProductViewModel : ObservableObject
 {
-    
+	//Essas propriedades representam os comandos que podem ser executados na ViewModel. 
+	//Os comandos são implementados utilizando a classe RelayCommand do Community Toolkit MVVM.
+	public RelayCommand AddToCart { get; set; }
+
+	public AddProductViewModel()
+	{
+		AddToCart = new RelayCommand(AddToCartCommand);
+	}
+
+	//O método AddToCartCommand() é chamado quando o comando Play é executado. Ele envia uma mensagem através do 
+	//mecanismo de mensagens para definir uma nova mídia selecionada e solicita a reprodução. Também define a 
+	//propriedade IsPlaying como true.
+	private void AddToCartCommand()
+	{
+		//Adição de item ao carrinho
+		var item = new CartItem()
+		{
+			ProductId = 2,
+			Name = "Batata frita",
+			Price = 2000,
+			Quantity = 1,
+			Observations = "Sem sal"
+		};
+		cart.InsertItem(item);
+	}
+
 }
