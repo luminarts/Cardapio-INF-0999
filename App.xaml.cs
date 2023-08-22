e7635f1eed1b4ecaa752198c8c2a931f3f3e2476
BrenoCSenna <100354416+BrenoCSenna@users.noreply.github.com>
2023-08-22 12:56:54 -0300

    Confirm_Order_screen

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,54 +5,13 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using CommunityToolkit.Mvvm.ComponentModel;
-using FastFoodly.Services;
-using FastFoodly.Stores;
-using FastFoodly.View;
-using FastFoodly.ViewModel;
-using Microsoft.Extensions.DependencyInjection;
 
-namespace FastFoodly
+namespace PedidoConfirmado
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Interação lógica para App.xaml
     /// </summary>
     public partial class App : Application
     {
-        //private readonly ServiceProvider _serviceProvider;
-        /*public App()
-        {
-            IServiceCollection services = new ServiceCollection();
-
-            services.AddSingleton<MainWindow>(provider => new MainWindow
-            {
-                DataContext = provider.GetRequiredService<MainViewModel>()
-            });
-            services.AddSingleton<MainViewModel>();
-            services.AddSingleton<HomeWindow>();
-            services.AddSingleton<HomeViewModel>();
-            services.AddSingleton<INavigationService, NavigationService>();
-
-            services.AddSingleton<Func<Type, ObservableObject>>(serviceProvider => viewModelType => (ObservableObject)serviceProvider.GetRequiredService(viewModelType));
-
-            _serviceProvider = services.BuildServiceProvider();
-        }*/
-
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-
-            NavigationStore navigationStore = new NavigationStore
-            {
-                CurrentViewModel = new HomeViewModel()
-            };
-            var mainWindow = new MainWindow()
-            {
-                DataContext = new MainViewModel(navigationStore)
-            };
-            mainWindow.Show();  
-        } 
-
-        string connectionString = ConfigurationManager.ConnectionStrings["MySqlConnection"].ConnectionString;
     }
 }
