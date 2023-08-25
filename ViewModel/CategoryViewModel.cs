13acb764cff0a6d76a20aa2b8d9a1d4cd2a11ff3
sathyagimenes <sathya.gimenes@gmail.com>
2023-08-24 22:16:03 -0300

    WIP: navigation with parameters

--- ViewModel/CategoryViewModel.cs
+++ ViewModel/CategoryViewModel.cs
@@ -3,14 +3,19 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using FastFoodly.Commands;
 using FastFoodly.Services;
 using FastFoodly.Stores;
+using FastFoodly.Models;
 
 namespace FastFoodly.ViewModel;
 
 public class CategoryViewModel : ViewModelBase
 {
     public ICommand NavigateToHome { get; set; }
-    public CategoryViewModel(NavigationStore navigationStore)
+    public string CategoryName { get; set; }
+    public CategoryViewModel(string categoryName, NavigationStore navigationStore)
     {
+        CategoryName = categoryName;
+        //Lista de itens de uma determinada categoria
+        List<Product> menuByCategory = database.ListByCategory(CategoryName);
         NavigateToHome = new NavigateCommand<HomeViewModel>(new NavigationService<HomeViewModel>(navigationStore, () => new HomeViewModel(navigationStore)));
     }
 }
