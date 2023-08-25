27e72c8cf6ae104e0526b6ca72778969491d63d3
Gustavo Borba <gustavo.borba2001@gmail.com>
2023-08-25 01:44:53 -0300

    feat: ItemsControl Inclusion bug: Resizing problem

---
+++ ViewModel/FoodItemsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastFoodly.ViewModel
+{
+    public class FoodItemsViewModel : ViewModelBase
+    {
+        public string Name { get; }
+        public string Description { get; }
+        //public double Price { get; }
+        public string ImagePath { get; }
+
+        public FoodItemsViewModel(string name, string description/*, double price*/, string imagePath)
+        {
+            Name = name;
+            Description = description;
+            //Price = price;
+            ImagePath = imagePath;
+        }
+    }
+}
--- ViewModel/HomeViewModel.cs
+++ ViewModel/HomeViewModel.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -12,9 +14,24 @@ public class HomeViewModel : ViewModelBase
 {
     private readonly NavigationStore _navigationStore;
     public ICommand NavigateToCategory { get; set; }
+    private readonly ObservableCollection<FoodItemsViewModel> _foodItemsViewModels;
+    public IEnumerable<FoodItemsViewModel> FoodItemsViewModels => _foodItemsViewModels;
+
     public HomeViewModel(NavigationStore navigationStore)
     {
         _navigationStore = navigationStore;
         NavigateToCategory = new NavigateCommand<CategoryViewModel>(new NavigationService<CategoryViewModel>(navigationStore, () => new CategoryViewModel(navigationStore)));
+
+        _foodItemsViewModels = new ObservableCollection<FoodItemsViewModel>()
+        {
+            new FoodItemsViewModel("X-Bacon","Um delicioso hamburguer de carne bovina de 200g com lascas de bacon crocantes e um queijo cheddar derretido de dar água na boca"/*, 29.99*/, "\\Assets\\Images\\front-view-burgers-stand.jpg"),
+            new FoodItemsViewModel("X-Egg","Um delicioso hamburguer de ovo bovino de 200g com lascas de ovo crocantes e um queijo cheddar derretido de dar água na boca"/*, 29.99*/, "\\Assets\\Images\\front-view-burgers-stand.jpg"),
+            new FoodItemsViewModel("X-Tudo","Um delicioso hamburguer de tudo bovino de 200g com lascas de tudo crocantes e um queijo cheddar derretido de dar água na boca"/*, 29.99*/, "\\Assets\\Images\\front-view-burgers-stand.jpg"),
+            new FoodItemsViewModel("X-Tudo","Um delicioso hamburguer de tudo bovino de 200g com lascas de tudo crocantes e um queijo cheddar derretido de dar água na boca"/*, 29.99*/, "\\Assets\\Images\\front-view-burgers-stand.jpg"),
+            new FoodItemsViewModel("X-Tudo","Um delicioso hamburguer de tudo bovino de 200g com lascas de tudo crocantes e um queijo cheddar derretido de dar água na boca"/*, 29.99*/, "\\Assets\\Images\\front-view-burgers-stand.jpg"),
+            new FoodItemsViewModel("X-Tudo","Um delicioso hamburguer de tudo bovino de 200g com lascas de tudo crocantes e um queijo cheddar derretido de dar água na boca"/*, 29.99*/, "\\Assets\\Images\\front-view-burgers-stand.jpg"),
+            new FoodItemsViewModel("X-Tudo","Um delicioso hamburguer de tudo bovino de 200g com lascas de tudo crocantes e um queijo cheddar derretido de dar água na boca"/*, 29.99*/, "\\Assets\\Images\\front-view-burgers-stand.jpg")
+        };
     }
+    
 }
