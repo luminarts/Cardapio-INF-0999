1c1cd69996adc726eff03a3b724521ee591c3ea8
Tiago Perrupato Antunes <tiagoperrupato@gmail.com>
2023-08-26 15:10:43 -0300

    WIP: navigate to Product and Category with dinamic list

--- ViewModel/HomeViewModel.cs
+++ ViewModel/HomeViewModel.cs
@@ -30,18 +30,27 @@ public class HomeViewModel : ViewModelBase
 
     public HomeViewModel(NavigationStore navigationStore)
     {
+        _navigationStore = navigationStore;
         //Manipulação da tabela do cardapio
         var database = new DatabaseService();
         //Lista todos os itens do menu
         menu = database.ListAllMenu();
 
         SearchItem = new RelayCommand<string>(SearchItemCommand);
-
-        _navigationStore = navigationStore;
+        NavigateToCategory = new CategoryCommand(
+            new ParameterNavigationService<string, CategoryViewModel>(
+                navigationStore, (parameter) => new CategoryViewModel(parameter, navigationStore)));
+        
+        NavigateToProduct = new ProductCommand(
+            new ParameterNavigationService<string, AddProductViewModel>(
+                navigationStore, (parameter) => new AddProductViewModel(parameter, navigationStore)));
+        
+        NavigateToCart = new NavigateCommand<CartViewModel>(
+            new NavigationService<CartViewModel>(navigationStore, () => new CartViewModel(navigationStore)));
 
         _foodItemsViewModels = new ObservableCollection<FoodItemsViewModel>()
         {
-            new FoodItemsViewModel("X-Bacon","Um delicioso hamburguer de carne bovina de 200g com lascas de bacon crocantes e um queijo cheddar derretido de dar água na boca"/*, 29.99*/, "Assets/Images/front-view-burgers-stand.jpg"),
+            new FoodItemsViewModel("Batata frita","Um delicioso hamburguer de carne bovina de 200g com lascas de bacon crocantes e um queijo cheddar derretido de dar água na boca"/*, 29.99*/, "Assets/Images/front-view-burgers-stand.jpg"),
             new FoodItemsViewModel("X-Egg","Um delicioso hamburguer de ovo bovino de 200g com lascas de ovo crocantes e um queijo cheddar derretido de dar água na boca"/*, 29.99*/, "Assets\\Images\\front-view-burgers-stand.jpg"),
             new FoodItemsViewModel("X-Tudo","Um delicioso hamburguer de tudo bovino de 200g com lascas de tudo crocantes e um queijo cheddar derretido de dar água na boca"/*, 29.99*/, "\\Assets\\Images\\front-view-burgers-stand.jpg"),
             new FoodItemsViewModel("X-Tudo","Um delicioso hamburguer de tudo bovino de 200g com lascas de tudo crocantes e um queijo cheddar derretido de dar água na boca"/*, 29.99*/, "\\Assets\\Images\\front-view-burgers-stand.jpg"),
@@ -54,10 +63,6 @@ public class HomeViewModel : ViewModelBase
             new FoodItemsViewModel("X-Tudo","Um delicioso hamburguer de tudo bovino de 200g com lascas de tudo crocantes e um queijo cheddar derretido de dar água na boca"/*, 29.99*/, "\\Assets\\Images\\front-view-burgers-stand.jpg"),
             new FoodItemsViewModel("X-Tudo","Um delicioso hamburguer de tudo bovino de 200g com lascas de tudo crocantes e um queijo cheddar derretido de dar água na boca"/*, 29.99*/, "\\Assets\\Images\\front-view-burgers-stand.jpg")
         };
-
-        NavigateToCategory = new CategoryCommand(new ParameterNavigationService<string, CategoryViewModel>(navigationStore, (parameter) => new CategoryViewModel(parameter, navigationStore)));
-        NavigateToProduct = new ProductCommand(new ParameterNavigationService<string, AddProductViewModel>(navigationStore, (parameter) => new AddProductViewModel(parameter, navigationStore)));
-        NavigateToCart = new NavigateCommand<CartViewModel>(new NavigationService<CartViewModel>(navigationStore, () => new CartViewModel(navigationStore)));
     }
 
     //O método SearchItemCommand() é chamado quando o comando SearchItem é executado. 
