12c45cac0d4aa6170be981d41c275665ae977522
sathyagimenes <sathya.gimenes@gmail.com>
2023-08-26 17:23:27 -0300

    fix: adjusted list to be observable

--- ViewModel/CartViewModel.cs
+++ ViewModel/CartViewModel.cs
@@ -12,13 +12,12 @@ namespace FastFoodly.ViewModel;
 
 public class CartViewModel : ViewModelBase
 {
-    private CartItem cartItem;
-
-	public CartItem CartItem
-	{
-		get { return cartItem; }
-		set { cartItem = value; }
-	}
+    private ObservableCollection<CartItem> _cartItems;
+    public ObservableCollection<CartItem> CartItems
+    {
+        get { return _cartItems; }
+        set => SetProperty(ref _cartItems, value);
+    }
     private readonly NavigationStore _navigationStore;
     public RelayCommand<int> DeleteItem { get; set; }
     public RelayCommand DeleteAllItems { get; set; }
@@ -28,32 +27,32 @@ public class CartViewModel : ViewModelBase
         //Manipulação da tabela Carrinho
         var cart = new DbCartService();
         //Lista todos os itens do carrinho
-        ObservableCollection<CartItem> cartItems = cart.ListAllItems();
+        CartItems = cart.ListAllItems();
 
-		DeleteItem = new RelayCommand<int>(DeleteItemCommand);
-		DeleteAllItems = new RelayCommand(DeleteAllItemsCommand);
-        
+        DeleteItem = new RelayCommand<int>(DeleteItemCommand);
+        DeleteAllItems = new RelayCommand(DeleteAllItemsCommand);
+
         _navigationStore = navigationStore;
         NavigateToHome = new NavigateCommand<HomeViewModel>(new NavigationService<HomeViewModel>(navigationStore, () => new HomeViewModel(navigationStore)));
     }
 
     //O método DeleteItemCommand() é chamado quando o comando DeleteItem é executado. 
-	private void DeleteItemCommand(int itemId)
-	{
-		var cart = new DbCartService();
+    private void DeleteItemCommand(int itemId)
+    {
+        var cart = new DbCartService();
 
-	    //Deleta um item especifico do carrinho
+        //Deleta um item especifico do carrinho
         cart.DeleteItem(itemId);
-	}
+    }
 
     //O método DeleteAllItemsCommand() é chamado quando o comando DeleteAllItems é executado. 
-	private void DeleteAllItemsCommand()
-	{
-		var cart = new DbCartService();
+    private void DeleteAllItemsCommand()
+    {
+        var cart = new DbCartService();
 
-	    //Deleta todos os itens do carrihno
+        //Deleta todos os itens do carrihno
         cart.DeleteAllItems();
-	}
+    }
 
     //adicionar serviço de salvar pedido no banco de dados
 }
